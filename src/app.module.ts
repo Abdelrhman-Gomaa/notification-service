12d157a0ee950465862d680794c7988152364a9c
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-10-18 10:24:16 +0300

    feat: database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KnexModule } from 'nestjs-knex';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    KnexModule.forRootAsync({
+      useFactory: () => ({
+        config: {
+          client: process.env.CLIENT,
+          connection: {
+            host: process.env.HOST,
+            port: +process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          },
+          useNullAsDefault: true,
+          migrations: {
+            directory: './src/database/migrations',
+          },
+        }
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
